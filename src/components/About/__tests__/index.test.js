aac42132ef6008ea2366560b08a3fa0aec9a549b
jessica-calderon <calderonjessica13@yahoo.com>
2022-09-06 00:29:11 -0500

    add About component tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+// jest afterEach global function
+afterEach(cleanup);
+describe('About component', () => {
+    // First Test
+    // verify that the component is rendering 
+    it('renders', () => {
+      render(<About />);
+    });
+    it('matches snapshot DOM node structure', () => {
+        // asFragment returns a snapshot of the About component
+        const { asFragment } = render(<About />);
+        // test & compare whether the expected and actual outcomes match 
+        expect(asFragment()).toMatchSnapshot();
+      });
+  
+    // Second Test
+  })

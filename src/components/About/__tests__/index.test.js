fbf556d28b8ffd3c5b7042f6de1ab4f4c22afdf9
jessica-calderon <calderonjessica13@yahoo.com>
2022-09-06 00:46:01 -0500

    add nav tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
 // jest afterEach global function
 afterEach(cleanup);
-describe('About component', () => {
-    // First Test
-    // verify that the component is rendering 
-    it('renders', () => {
-      render(<About />);
-    });
-    it('matches snapshot DOM node structure', () => {
-        // asFragment returns a snapshot of the About component
-        const { asFragment } = render(<About />);
-        // test & compare whether the expected and actual outcomes match 
-        expect(asFragment()).toMatchSnapshot();
-      });
-  
-    // Second Test
-  })
+describe("About component", () => {
+  // First Test
+  // verify that the component is rendering
+  it("renders", () => {
+    render(<About />);
+  });
+  // Second test
+  it("matches snapshot DOM node structure", () => {
+    // asFragment returns a snapshot of the About component
+    const { asFragment } = render(<About />);
+    // test & compare whether the expected and actual outcomes match
+    expect(asFragment()).toMatchSnapshot();
+  });
+});

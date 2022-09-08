cc1d1f1c979d8d5e2b4cf1b993027227d5c231cf
jessica-calderon <calderonjessica13@yahoo.com>
2022-09-07 23:51:22 -0500

    add contact form

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -7,26 +7,25 @@ import Nav from '..';
 
 afterEach(cleanup);
 
-describe('Nav component', () => {
+describe('Nav component renders', () => {
   it('renders', () => {
     render(<Nav />);
   });
 
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-
+    
     expect(asFragment()).toMatchSnapshot();
   });
 })
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
+  const { getByLabelText } = render(<Nav />);
 
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})
+})  
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
